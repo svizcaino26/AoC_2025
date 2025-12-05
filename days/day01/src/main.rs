4ec1991dca7447643bb91d0d73f535adedbc4f9a
svizcaino26 <svizcaino26@gmail.com>
2025-12-04 19:38:32 -0500

    finish day 1 part 2

--- days/day01/src/main.rs
+++ days/day01/src/main.rs
@@ -10,34 +10,45 @@ fn main() {
     for r in rotations {
         let mut parts = r.chars();
         let direction = parts.next().unwrap();
-        let clicks: i32 = parts.collect::<String>().parse().unwrap();
+        let mut clicks: i32 = parts.collect::<String>().parse().unwrap();
+        if clicks > 100 {
+            perform_cycles(&mut clicks, &mut password);
+        }
         rotate_dial(&mut position, &mut password, clicks, &direction);
     }
     println!("The password is: {password}");
 }
 
+fn perform_cycles(clicks: &mut i32, password: &mut i32) {
+    *password += *clicks / 100;
+    *clicks = *clicks % 100;
+}
+
 fn rotate_dial(position: &mut i32, password: &mut i32, clicks: i32, direction: &char) {
+    let old = *position;
     match direction {
         'R' => {
             *position += clicks;
-            while *position > 99 {
-                *position -= 100;
-            }
-            if *position == 0 {
+            if *position > 99 {
                 *password += 1;
+                *position -= 100;
+                return;
             }
-            return;
         }
         'L' => {
             *position -= clicks;
-            while *position < 0 {
+            if *position < 0 {
+                if old != 0 {
+                    *password += 1;
+                }
                 *position += 100;
+                return;
             }
-            if *position == 0 {
-                *password += 1;
-            }
-            return;
         }
         _ => (),
+    }
+
+    if *position == 0 {
+        *password += 1;
     }
 }
