fc79efdbb4f7e27e925243b4c3edf4baa6e26ffc
svizcaino26 <svizcaino26@gmail.com>
2025-12-06 18:42:16 -0500

    finish day 3 part 2
    the new code allows for selecting combinations of n numbers by just
    switching the variable remaining_positions

--- days/day03/src/main.rs
+++ days/day03/src/main.rs
@@ -1,26 +1,60 @@
+use std::collections::HashSet;
+
 use common::load_vec_from_file;
 
 fn main() {
     // let contents = load_vec_from_file("input/day03/sample.txt").unwrap();
     let contents = load_vec_from_file("input/day03/puzzle.txt").unwrap();
     let mut result = 0;
+    let mut selected_batteries = String::new();
+    let mut cursor: usize;
+    let mut remaining_batteries;
+    let mut remaining_positions;
+    let mut digits: Vec<u32> = Vec::new();
 
     for s in contents {
-        let len = s.len();
-        let l = get_highest_value_as_string(&s[..(len - 1)]);
-        let i = s.find(&l[..]).unwrap();
-        // dbg!(i);
-        let r = get_highest_value_as_string(&s[(i + 1)..]);
-        result += (l + &r).parse::<i32>().unwrap();
+        selected_batteries.clear();
+        cursor = 0;
+        remaining_positions = 12;
+        remaining_batteries = &s[cursor..];
+        digits.clear();
+
+        while remaining_positions > 0 {
+            remaining_batteries = &remaining_batteries[cursor..];
+            remaining_positions -= 1;
+            digits = extract_unique_digits(&s[cursor..]);
+            digits.sort_unstable_by(|a, b| b.cmp(a));
+
+            for d in &digits {
+                if let Some(i) = get_highest_value_as_string(
+                    remaining_batteries,
+                    remaining_positions,
+                    &d.to_string(),
+                ) {
+                    selected_batteries += &remaining_batteries[i..=i];
+                    cursor = i + 1;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        }
+        result += selected_batteries.parse::<u64>().unwrap();
     }
-    println!("{result}");
+    println!("Result: {result}");
 }
 
-fn get_highest_value_as_string(s: &str) -> String {
-    // dbg!(s);
+fn get_highest_value_as_string(s: &str, remaining: usize, num: &str) -> Option<usize> {
+    let len = s.len();
+    s.chars()
+        .position(|c| c.to_string() == num)
+        .take_if(|p| (len - 1) - *p >= remaining)
+}
+
+fn extract_unique_digits(s: &str) -> Vec<u32> {
     s.chars()
         .map(|c| c.to_digit(10).unwrap())
-        .max()
-        .unwrap()
-        .to_string()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect()
 }
