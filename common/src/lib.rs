40ca782d140b9012edd148e8efba1e6cd66525d5
svizcaino26 <svizcaino26@gmail.com>
2025-12-06 18:40:30 -0500

    add trimming ot strings to clean input before returning the result

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
 pub fn load_vec_from_file(path: &str) -> anyhow::Result<Vec<String>> {
-    let contents = fs::read_to_string(path)?;
-    Ok(contents.lines().map(|x| x.into()).collect())
+    let contents: String = fs::read_to_string(path)?.trim().into();
+    Ok(contents.lines().map(|x| x.trim().into()).collect())
 }
