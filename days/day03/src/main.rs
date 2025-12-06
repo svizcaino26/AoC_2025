298d3bbe2a0aa42d0d62c6889bb25796758e1ec5
svizcaino26 <svizcaino26@gmail.com>
2025-12-06 01:42:26 -0500

    finish day 3 part 1

--- days/day03/src/main.rs
+++ days/day03/src/main.rs
@@ -1,3 +1,26 @@
+use common::load_vec_from_file;
+
 fn main() {
-    println!("Hello, world!");
+    // let contents = load_vec_from_file("input/day03/sample.txt").unwrap();
+    let contents = load_vec_from_file("input/day03/puzzle.txt").unwrap();
+    let mut result = 0;
+
+    for s in contents {
+        let len = s.len();
+        let l = get_highest_value_as_string(&s[..(len - 1)]);
+        let i = s.find(&l[..]).unwrap();
+        // dbg!(i);
+        let r = get_highest_value_as_string(&s[(i + 1)..]);
+        result += (l + &r).parse::<i32>().unwrap();
+    }
+    println!("{result}");
+}
+
+fn get_highest_value_as_string(s: &str) -> String {
+    // dbg!(s);
+    s.chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .max()
+        .unwrap()
+        .to_string()
 }
