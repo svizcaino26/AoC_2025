f0855906ce72b7214ef6992c534b92faa34340d0
svizcaino26 <svizcaino26@gmail.com>
2025-12-12 00:11:46 -0500

    create function tu load a flatgrid from file
    flatgrid is more exficient in terms of allocation per row of data, the
    become only one allocation perfile instead of one allocation per row

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,3 +4,26 @@ pub fn load_vec_from_file(path: &str) -> anyhow::Result<Vec<String>> {
     let contents: String = fs::read_to_string(path)?.trim().into();
     Ok(contents.lines().map(|x| x.trim().into()).collect())
 }
+
+pub fn load_grid(path: &str) -> anyhow::Result<FlatGrid> {
+    let contents = fs::read_to_string(path)?;
+    let height = contents.lines().collect::<Vec<_>>().len();
+    let width = contents.lines().collect::<Vec<_>>()[0].len();
+    let cells = contents
+        .lines()
+        .flat_map(|line| line.as_bytes().to_vec())
+        .collect();
+
+    Ok(FlatGrid {
+        width,
+        height,
+        cells,
+    })
+}
+
+#[derive(Debug)]
+pub struct FlatGrid {
+    width: usize,
+    height: usize,
+    cells: Vec<u8>,
+}
